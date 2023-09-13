639ca283eae7737d3875c98f33b4e7cc7f939464
cryptnology <jamie@cryptnology.dev>
2023-09-13 09:47:19 +0100

    Add next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,26 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 const NavBar = () => {
+  const { data: session, status } = useSession();
+
   return (
-    <div className="flex bg-slate-200 p-5">
-      <Link className="mr-5" href="/">
-        Next.js
-      </Link>
+    <div className="flex bg-slate-200 p-5 space-x-3">
+      <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
+      {status === 'authenticated' && (
+        <div>
+          {session.user!.name}
+          <Link className="ml-3" href="/api/auth/signout">
+            Sign Out
+          </Link>
+        </div>
+      )}
+      {status === 'unauthenticated' && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { getServerSession } from 'next-auth';
 import Link from 'next/link';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-const Home = () => {
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
     </main>
   );
